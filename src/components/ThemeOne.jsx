7259038871d864237e1f8c556c533de25bff97ef
luca <lgorvin78@gmail.com>
2022-09-13 16:54:47 +0100

    adding functionality

--- src/components/ThemeOne.jsx
+++ src/components/ThemeOne.jsx
@@ -2,7 +2,14 @@ import { useState } from "react";
 import React from "react";
 
 const ThemeOne = () => {
-  const [calcNum, setCalcnum] = useState(0);
+  const [calcNum, setCalcnum] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["/", "x", "+", "-", "."];
+
+  const updateCalc = (value) => {
+    setCalcnum(calcNum + value);
+  };
   const handleClick = (e) => {
     setCalcnum(e.target.value);
   };
@@ -11,8 +18,13 @@ const ThemeOne = () => {
     <div>
       <div className="flex justify-center w-screen">
         <div className="h-[140px] w-[530px] rounded-lg bg-slate-800">
-          <h1 className="flex items-center h-full text-4xl text-white float-right mr-4">
-            {calcNum}
+          <h1 className="flex items-center h-full text-5xl text-white float-right mr-4">
+            {result ? (
+              <span className="text-gray-500 text-md mr-5">(0)</span>
+            ) : (
+              ""
+            )}
+            {calcNum || "0"}
           </h1>
         </div>
       </div>
@@ -20,22 +32,19 @@ const ThemeOne = () => {
         <div className="h-[440px] w-[530px] mt-6 rounded-lg bg-slate-700">
           <div className="grid place-items-center grid-cols-4 gap-1 m-3 mt-6">
             <button
-              value={7}
-              onClick={handleClick}
+              onClick={() => updateCalc("7")}
               className="h-[65px] w-[90px] text-3xl bg-slate-200 rounded-lg"
             >
               7
             </button>
             <button
-              value={8}
-              onClick={handleClick}
+              onClick={() => updateCalc("8")}
               className="h-[65px] w-[90px] text-3xl bg-slate-200 rounded-lg"
             >
               8
             </button>
             <button
-              value={9}
-              onClick={handleClick}
+              onClick={() => updateCalc("9")}
               className="h-[65px] w-[90px] text-3xl  bg-slate-200 rounded-lg"
             >
               9
@@ -44,22 +53,19 @@ const ThemeOne = () => {
               DEL
             </button>
             <button
-              value={4}
-              onClick={handleClick}
+              onClick={() => updateCalc("4")}
               className="h-[65px] w-[90px] text-3xl mt-4 bg-slate-200 rounded-lg"
             >
               4
             </button>
             <button
-              value={5}
-              onClick={handleClick}
+              onClick={() => updateCalc("5")}
               className="h-[65px] w-[90px] text-3xl mt-4  bg-slate-200 rounded-lg"
             >
               5
             </button>
             <button
-              value={6}
-              onClick={handleClick}
+              onClick={() => updateCalc("6")}
               className="h-[65px] w-[90px] text-3xl mt-4  bg-slate-200 rounded-lg"
             >
               6
@@ -68,43 +74,51 @@ const ThemeOne = () => {
               +
             </button>
             <button
-              value={1}
-              onClick={handleClick}
+              onClick={() => updateCalc("1")}
               className="h-[65px] w-[90px] text-3xl mt-4 bg-slate-200 rounded-lg"
             >
               1
             </button>
             <button
-              value={2}
-              onClick={handleClick}
+              onClick={() => updateCalc("2")}
               className="h-[65px] w-[90px] text-3xl mt-4  bg-slate-200 rounded-lg"
             >
               2
             </button>
             <button
-              value={3}
-              onClick={handleClick}
+              onClick={() => updateCalc("3")}
               className="h-[65px] w-[90px] text-3xl mt-4  bg-slate-200 rounded-lg"
             >
               3
             </button>
-            <button className="h-[65px] w-[90px] text-3xl mt-4  bg-slate-200 rounded-lg">
+            <button
+              onClick={() => updateCalc("-")}
+              className="h-[65px] w-[90px] text-3xl mt-4  bg-slate-200 rounded-lg"
+            >
               -
             </button>
-            <button className="h-[65px] w-[90px] text-3xl mt-4 bg-slate-200 rounded-lg">
+            <button
+              onClick={() => updateCalc(".")}
+              className="h-[65px] w-[90px] text-3xl mt-4 bg-slate-200 rounded-lg"
+            >
               .
             </button>
             <button
-              value={0}
-              onClick={handleClick}
+              onClick={() => updateCalc("0")}
               className="h-[65px] w-[90px] text-3xl mt-4  bg-slate-200 rounded-lg"
             >
               0
             </button>
-            <button className="h-[65px] w-[90px] text-3xl mt-4  bg-slate-200 rounded-lg">
+            <button
+              onClick={() => updateCalc("/")}
+              className="h-[65px] w-[90px] text-3xl mt-4  bg-slate-200 rounded-lg"
+            >
               /
             </button>
-            <button className="h-[65px] w-[90px] text-3xl mt-4  bg-slate-200 rounded-lg">
+            <button
+              onClick={() => updateCalc("x")}
+              className="h-[65px] w-[90px] text-3xl mt-4  bg-slate-200 rounded-lg"
+            >
               x
             </button>
           </div>
@@ -112,7 +126,10 @@ const ThemeOne = () => {
             <button className="h-[65px] w-[90%] text-3xl text-white  bg-cyan-700 rounded-lg">
               RESET
             </button>
-            <button className="h-[65px] w-[90%] text-3xl text-white  bg-red-500 rounded-lg">
+            <button
+              onClick={() => updateCalc("=")}
+              className="h-[65px] w-[90%] text-3xl text-white  bg-red-500 rounded-lg"
+            >
               =
             </button>
           </div>
